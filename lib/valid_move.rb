763394cac49cafa61cfef0530de8c4bb3e7b944e
Romano Livi <romano_livi@outlook.com>
2020-12-09 17:04:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,16 +5,14 @@ def valid_move?(board, index)
     return false
   end
   
+end
 
-
-
-  def position_taken?(board, index)
+def position_taken?(board, index)
       if board[index] == " " || board[index] == "" || board[index] == nil
         return false 
       elsif board[index] == "X" || board[index] == "O"
         return true
       end
-  end
-  
 end
+  
 
