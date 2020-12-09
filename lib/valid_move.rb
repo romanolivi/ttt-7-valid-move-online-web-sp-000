5e1bd5ad782bdae42f8e67ed48d0063365d5c841
Romano Livi <romano_livi@outlook.com>
2020-12-09 16:54:55 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,6 @@
 def valid_move?(board, index)
-  if !position_taken(bo)
+  if !position_taken(board, index) && index.between?(0, 8)
+    return true
   elsif index.between?(0, 8)
     return true
   end
