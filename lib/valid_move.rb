b1c1fa5afbfbf2365dc876a78170137e41515f89
Romano Livi <romano_livi@outlook.com>
2020-12-09 16:56:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,13 +8,13 @@ def valid_move?(board, index)
 
 
 
-    def position_taken?(board, index)
-        if board[index] == " " || board[index] == "" || board[index] == nil
-          return false 
-        elsif board[index] == "X" || board[index] == "O"
-          return true
-        end
+def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return false 
+    elsif board[index] == "X" || board[index] == "O"
+      return true
     end
+end
   
 end
 
